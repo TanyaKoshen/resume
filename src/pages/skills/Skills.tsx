982da191600879d9c2e33787c4e4be984081e854
Tatiana Peretyatko <117319673+TanyaKoshen@users.noreply.github.com>
2023-09-04 17:39:01 -0400

    minor fix

--- src/pages/skills/Skills.tsx
+++ src/pages/skills/Skills.tsx
@@ -8,60 +8,70 @@ import {GrGraphQl, GrNode} from "react-icons/gr";
 import {SiExpress} from "react-icons/si";
 import {BsFiletypeCss} from "react-icons/bs";
 import {AiFillHtml5} from "react-icons/ai"
+import {nanoid} from "nanoid";
 
 
 const Skills = () => {
 
     const skills = [
         {
+            id: nanoid(),
             skill: 'Javascript',
             range: 90,
             color: 'rgba(199, 152, 34, 0.91)',
             label: <RiJavascriptLine/>
         },
         {
+            id: nanoid(),
             skill: 'Typescript',
             range: 75,
             color: 'blue',
             label: <BiLogoTypescript/>
         },
         {
+            id: nanoid(),
             skill: 'React',
             range: 80,
             color: 'cornflowerblue',
             label: <FaReact/>
         },
         {
+            id: nanoid(),
             skill: 'Mongo',
             range: 70,
             color: 'green',
             label: <SiMongodb/>
         },
         {
+            id: nanoid(),
             skill: 'NodeJS',
             range: 65,
             color: 'darkgreen',
             label: <GrNode/>
         },
         {
+            id: nanoid(),
             skill: 'Express',
             range: 80,
             color: 'black',
             label: <SiExpress/>
         },
         {
+            id: nanoid(),
             skill: 'CSS',
             range: 75,
             color: 'darkblue',
             label: <BsFiletypeCss/>
         },
         {
+            id: nanoid(),
             skill: 'HTML',
             range: 85,
             color: 'darkred',
             label: <AiFillHtml5/>
         },
         {
+            id: nanoid(),
             skill: 'Apollo GraphQL',
             range: 85,
             color: 'rgb(205,56,205)',
@@ -87,12 +97,12 @@ const Skills = () => {
                 My stack is always evolving, and I'm dedicated to learning beyond my work environment.
             </p>
             <div className={cl['skills-columns']}>
-                {skills.map(({skill, range, color, label}) =>
-                    <div className={cl['skills-label-container']}>
+                {skills.map(({id, skill, range, color, label}) =>
+                    <div
+                        key={id}
+                        className={cl['skills-label-container']}>
                         <div style={{color: color}}
-                             className={cl.skills}
-                             key={skill}
-                        >
+                             className={cl.skills}>
                             {React.cloneElement(label, {className: cl.label})}
                         </div>
                         <div className={cl.skills}>
