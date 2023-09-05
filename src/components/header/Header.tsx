bb609440d6c851f83878ae3300e4efb26c200ddf
Tatiana Peretyatko <117319673+TanyaKoshen@users.noreply.github.com>
2023-09-05 17:53:44 -0400

    updates into ed, added time to header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,27 +1,38 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import cl from './Header.module.css'
 import {Link} from "react-router-dom";
 
 const Header = () => {
     const [active, setActive] = useState('')
+
+    const [currentTime, setCurrentTime] = useState(new Date());
+
+    useEffect(()=>{
+        const intevalId = setInterval(()=>{
+            setCurrentTime(new Date());
+        },1000);
+        return ()=>clearInterval(intevalId);
+    },[]);
+
     return (
-        <div className={cl.header}>
-            <div >
+        <div className={cl['header-container']}>
+            <div className={cl.header}>
                 <Link to={'score'}
                       className={cl['score-link']}>
                     Scoreboard
                 </Link>
+                {currentTime.toLocaleTimeString()}
             </div>
-            <div
-                onClick={()=>setActive('Login')}
-                className={cl['header-item']}>
-                Login
-            </div>
-            <div
-                onClick={()=>setActive('Sign up')}
-                className={cl['header-item']}>
-                Sign up
-            </div>
+            {/*<div*/}
+            {/*    onClick={()=>setActive('Login')}*/}
+            {/*    className={cl['header-item']}>*/}
+            {/*    Login*/}
+            {/*</div>*/}
+            {/*<div*/}
+            {/*    onClick={()=>setActive('Sign up')}*/}
+            {/*    className={cl['header-item']}>*/}
+            {/*    Sign up*/}
+            {/*</div>*/}
         </div>
     );
 };
--- src/pages/score/model.ts
+++ src/pages/score/model.ts
@@ -3,7 +3,7 @@ export interface ICategory{
     id: string;
     metric: string;
     description: string;
-    score: number[] | never[];
+    score: number[];
     comments: string;
     average: number;
 }
