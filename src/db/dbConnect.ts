8b2def264be991834287940e9ad105ceaa3220c1
Tatiana Peretyatko <117319673+TanyaKoshen@users.noreply.github.com>
2023-09-08 09:50:55 -0400

    fixed styles in grid score card

--- src/db/dbConnect.ts
+++ src/db/dbConnect.ts
@@ -14,6 +14,15 @@ const firebaseConfig = {
     measurementId: "G-YXW0JH2E9Q"
 };
 
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_APP_ID,
+//     measurementId: process.env.MEASUREMENT_ID
+// };
 
 
 // Initialize Firebase
--- src/pages/score/card/Scorecard.tsx
+++ src/pages/score/card/Scorecard.tsx
@@ -4,29 +4,28 @@ import cl from './Scorecard.module.css';
 import Rating from "../rating/Rating";
 
 
-interface IScorecardProps{
+interface IScorecardProps {
     category: ICategory;
-    handleRatingUpdate: (id: string, newRating: number)=> void;
+    handleRatingUpdate: (id: string, newRating: number) => void;
     isActive: boolean;
 }
 
-const Scorecard = ({category, handleRatingUpdate, isActive}:IScorecardProps ) => {
+const Scorecard = ({category, handleRatingUpdate, isActive}: IScorecardProps) => {
 
     return (
         // <div className={cl['card-container']}>
-            <div className={cl['score-grid']}>
+        <div className={cl['score-grid']}>
             <h5>{category.metric}</h5>
             <div className={cl.avg}>
                 <Rating
-                        categoryId={category.id}
-                        handleRatingUpdate={handleRatingUpdate}
-                        isActive={isActive}
-                        average={Math.round(category.average)}
+                    categoryId={category.id}
+                    handleRatingUpdate={handleRatingUpdate}
+                    isActive={isActive}
+                    average={Math.round(category.average)}
                 />
             </div>
-                {!isActive && <span>({category.average.toFixed(1)})</span>}
+            {!isActive && <span>({category.average.toFixed(1)})</span>}
             <h4>{category.description}</h4>
-            <div>{category.comments}</div>
         </div>
 
 
